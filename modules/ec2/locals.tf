0bab8009c758333fd4d8aed9a7cf827d72078080
abasimdev <abasimdev@gmail.com>
2026-01-20 13:22:08 +0530

    Added ECR module and main TF files

--- modules/ec2/locals.tf
+++
@@ -1,9 +0,0 @@
-
-
-locals {
-  # ec2-name       = "OnlineBookStore-EC2"
-  user-data-file = "${path.module}/scripts/user-data.sh"
-  tomcat-script  = "${path.module}/scripts/tomcat.sh"
-  docker-script  = "${path.module}/scripts/docker.sh"
-
-}
--- modules/ec2/outputs.tf
+++
@@ -1,5 +0,0 @@
-
-# output "sg" {
-#     value = data.aws_security_group.tomcat-sg
-
-# }
